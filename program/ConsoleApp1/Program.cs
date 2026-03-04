80fb27a3f378dba42a24f422e4b1d95b758f7dbf
Andrij Myrončuk <andrijmyroncuk@gmail.com>
2026-03-04 10:06:24 +0100

    04.03

--- program/ConsoleApp1/Program.cs
+++ program/ConsoleApp1/Program.cs
@@ -1,5 +1,4 @@
-﻿
-namespace ConsoleApp13
+﻿namespace pexeso2
 {
     internal class Program
     {
@@ -10,6 +9,7 @@ namespace ConsoleApp13
             {
                 string[] znaky = { ":)", ":3", ":/", ":>", ":<", ":P" };
                 int[,] pexeso = new int[3, 4];
+                bool[,] odkryte = new bool[3, 4];
                 int[] kontrola = { 0, 0, 0, 0, 0, 0 };
                 bool loop = true;
                 int R;
@@ -34,10 +34,87 @@ namespace ConsoleApp13
                         } while (loop);
                     }
                 }
-                VypisPole(pexeso, znaky);
-            } while (!cykl);
+
+                int nalezeno = 0;
+                do
+                {
+                    Console.Clear();
+                    VypisPole(pexeso, znaky, odkryte);
+
+                    Console.WriteLine("První políčko - řádek:");
+                    int row1 = int.Parse(Console.ReadLine()) - 1;
+
+                    if (row1 > 4 || row1 < 0)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine("První políčko - sloupec:");
+                    int col1 = int.Parse(Console.ReadLine()) - 1;
+
+                    if (col1 > 3 || col1 < 0)
+                    {
+                        break;
+                    }
+
+                    if (odkryte[row1, col1] == true)
+                    {
+                        continue;
+                    }
+
+                    odkryte[row1, col1] = true;
+                    //string znak1 = znaky[pexeso[row1, col1]];
+
+                    Console.Clear();
+                    VypisPole(pexeso, znaky, odkryte);
+
+                    Console.WriteLine("Druhé políčko - řádek:");
+                    int row2 = int.Parse(Console.ReadLine()) - 1;
+
+                    if (row2 > 4 || row2 < 0)
+                    {
+                        odkryte[row1, col1] = false;
+                        break;
+                    }
+
+                    Console.WriteLine("Druhé políčko - sloupec:");
+                    int col2 = int.Parse(Console.ReadLine()) - 1;
+
+                    if (col2 > 3 || row2 < 0)
+                    {
+                        odkryte[row1, col1] = false;
+                        break;
+                    }
+
+                    if (odkryte[row2, col2] == true)
+                    {
+                        continue;
+                    }
+                    odkryte[row2, col2] = true;
+
+                    Console.Clear();
+                    VypisPole(pexeso, znaky, odkryte);
+
+                    if (pexeso[row1, col1] == pexeso[row2, col2] && !((row1) == (row2) && (col1) == (col2)))
+                    {
+                        Console.WriteLine("Shoda");
+                        Thread.Sleep(2000);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Neshoda");
+                        Thread.Sleep(2000);
+                        odkryte[row1, col1] = false;
+                        odkryte[row2, col2] = false;
+                    }
+
+
+                } while (nalezeno < 6);
+
+            } while (cykl);
         }
-        public static void VypisPole(int[,] pexeso, string[] znaky)
+        public static void VypisPole(int[,] pexeso, string[] znaky, bool[,] odkryte)
         {
             Console.WriteLine("   1  2  3  4");
             for (int i = 0; i < pexeso.GetLength(0); i++)
@@ -45,8 +122,14 @@ namespace ConsoleApp13
                 Console.Write(i + 1 + " ");
                 for (int j = 0; j < pexeso.GetLength(1); j++)
                 {
-
-                    Console.Write(znaky[pexeso[i, j] - 1] + " ");
+                    if (odkryte[i, j] == false)
+                    {
+                        Console.Write(" * ");
+                    }
+                    else
+                    {
+                        Console.Write(znaky[pexeso[i, j] - 1] + " ");
+                    }
                 }
                 Console.WriteLine();
             }
--- program/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ program/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa33ad922dc8679dfc2b1fb0046a94f25016b1ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad631fcce87b7cf44b345f5f0a498939c961fc97")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
